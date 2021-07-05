8d9360a6eea83f197963f80986ed3d26b1d57f8a
alejandro <ale.fuentes.misc@gmail.com>
2021-07-05 20:34:50 -0300

    refactor - star project

---
+++ src/main/java/edu/alefu/babystep/BabystepApplication.java
@@ -0,0 +1,13 @@
+package edu.alefu.babystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabystepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabystepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/alefu/babystep/BabystepApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alefu.babystep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabystepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
